8c7ed92be105b74340f236779e5801d96bfef35e
Nolan Melhart <nkmelhart@gmail.com>
2021-02-14 23:40:22 -0600

    Scaffold final

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,8 @@
     <Header />
     <Main>
       <h4 class="text-center" v-if="todos.length==0">Enter an task to get the list started!</h4>
-      <section class="space-y-4" v-if="pendingTodos.length>0">
-        <h3 class="text-3xl text-green-800">Pending Items : {{pendingTodos.length}}</h3>
-        <ul class="space-y-4">
-          <li v-for="todo in pendingTodos" :key="todo.id" 
-          class="bg-white rounded-xl text-2xl py-2 px-4 font-light text-center hover:text-white hover:bg-green-800 transition transform ease-in-out hover:scale-110 duration-500 cursor-pointer" 
-          @click="changeStatus(todo.id)"
-          >
-          {{todo.text}}
-          </li>
-        </ul>
-      </section>
-      <section class="space-y-4" v-if="completedTodos.length>0">
-        <h3 class="text-3xl text-red-800">Completed Items : {{completedTodos.length}}</h3>
-        <ul class="space-y-4">
-          <li v-for="todo in completedTodos" :key="todo.id" 
-          class="bg-white rounded-xl text-2xl py-2 px-4 font-light text-center hover:text-white hover:bg-red-800 transition transform ease-in-out hover:scale-110 duration-500 cursor-pointer"
-          @click="changeStatus(todo.id)"
-          >
-          {{todo.text}}
-          </li>
-        </ul>
-      </section>
+      <ListTodos color="green" title="Pending Items" :todos="pendingTodos"/>
+      <ListTodos color="red" title="Completed Items" :todos="completedTodos"/>
     </Main>
   </div>
 </template>
@@ -32,5 +12,6 @@
 <script setup>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
+import ListTodos from './components/ListTodos.vue'
 import {pendingTodos, completedTodos, changeStatus, todos} from './helpers/useTodos.js'
 </script>
---
+++ src/components/ListTodos.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="space-y-4" v-if="todos.length>0">
+        <h3 class="text-3xl" 
+        :class="color==='green' ? 'text-green-800' : 'text-red-800'">
+        {{title}} : {{todos.length}}</h3>
+        <ul class="space-y-4">
+          <li v-for="todo in todos" :key="todo.id" 
+          class="bg-white rounded-xl text-2xl py-2 px-4 font-light text-center hover:text-white transition transform ease-in-out hover:scale-110 duration-500 cursor-pointer" 
+          :class="color==='green' ? 'hover:bg-green-800' : 'hover:bg-red-800'"
+          @click="changeStatus(todo.id)"
+          >
+          {{todo.text}}
+          </li>
+        </ul>
+    </section>
+</template>
+
+<script setup>
+import {defineProps} from 'vue'
+import {changeStatus} from '../helpers/useTodos.js'
+
+defineProps ({
+    color: {
+        type: String,
+        default : 'green',
+    },
+    title: {
+        type: String,
+        default: 'Todo Title',
+    },
+    todos: {
+        type: Array,
+        default: [],
+    }
+
+})
+</script>
